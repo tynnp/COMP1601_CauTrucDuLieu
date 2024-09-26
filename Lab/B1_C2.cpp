56c29f703ea89ff9c1b45380184082e34796c4c9
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-09-26 11:25:09 +0700

    comment hàm add, find và getAt

--- Lab/B1_C2.cpp
+++ Lab/B1_C2.cpp
@@ -4,8 +4,8 @@ using namespace std;
 template<class DataType>
 class Array {
 private:
-    DataType* _items;
-    int _iLength;
+    DataType* _items;       // Mảng chứa các phần tử
+    int _iLength;           // Số lượng phần tử hiện tại
 
 public:
     Array();
@@ -14,11 +14,11 @@ public:
     int size();
     bool empty();
     void clear();
-    bool add(DataType item);
-    DataType getAt(int index);
-    int find(DataType item, int index = 0);
-    bool removeAt(int index);
-    bool removeAll(DataType item);
+    bool add(DataType item);                    // Thêm phần tử vào cuối mảng
+    DataType getAt(int index);                  // Lấy giá trị phần tử tại chỉ số index
+    int find(DataType item, int index = 0);     // Tìm chỉ số đầu tiên xuất hiện của phần tử
+    bool removeAt(int index);                   // Xóa phần tử tại chỉ số index
+    bool removeAll(DataType item);              // Xóa toàn bộ phần tử có giá trị item
 };
 
 int main() {
@@ -85,6 +85,12 @@ void Array<DataType>::clear() {
     _iLength = 0;
 }
 
+/***************************************************************************
+* @Description hàm thêm phần tử có giá trị item vào cuối mảng.
+* @param item: giá trị của phần tử cần thêm.
+* @attention: 
+* @return true nếu thêm thành công, false nếu số lượng phần tử đã tối đa.
+****************************************************************************/
 template<class DataType>
 bool Array<DataType>::add(DataType item) {
     if (_iLength < 100) {
@@ -96,11 +102,24 @@ bool Array<DataType>::add(DataType item) {
     return false;
 }
 
+/***************************************************************************
+* @Description hàm trả về giá trị tại vị trí index.
+* @param index: chỉ số của phần tử cần lấy giá trị.
+* @attention: 
+* @return giá trị tại vị trí index.
+****************************************************************************/
 template<class DataType>
 DataType Array<DataType>::getAt(int index) {
     return _items[index];
 }
 
+/***************************************************************************
+* @Description tìm chỉ số của phần tử bắt đầu từ index.
+* @param item: giá trị của phần tử cần tìm.
+* @param index: chỉ số bắt đầu tìm, mặc định là 0.
+* @attention: 
+* @return chỉ số đầu tiên tìm thấy của phần tử đó trong mảng, -1 nếu không tìm thấy.
+****************************************************************************/
 template<class DataType>
 int Array<DataType>::find(DataType item, int index) {
     for (int i = index; i < _iLength; i++)
@@ -112,6 +131,7 @@ int Array<DataType>::find(DataType item, int index) {
 /***************************************************************************
 * @Description xóa phần tử tại vị trí index.
 * @param index: vị trí cần xóa.
+* @attention: 
 * @return true nếu xóa thành công, false nếu vị trí không hợp lệ.
 ****************************************************************************/
 template<class DataType>
@@ -128,6 +148,7 @@ bool Array<DataType>::removeAt(int index) {
 /***************************************************************************
 * @Description xóa tất cả phần tử có giá trị bằng với item.
 * @param item: giá trị phần tử cần xóa.
+* @attention: 
 * @return true nếu xóa thành công, false nếu không xóa phần tử nào.
 ****************************************************************************/
 template<class DataType>
